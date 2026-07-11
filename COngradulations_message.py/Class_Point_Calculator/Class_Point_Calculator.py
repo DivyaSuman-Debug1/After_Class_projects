06e5b6ba9a398d12f5ed19182b14c231a4ea5bff
Divya Suman <divya2suman@gmail.com>
2026-07-11 11:42:57 +0400

    Done

--- COngradulations_message.py/Class_Point_Calculator/Class_Point_Calculator.py
+++ COngradulations_message.py/Class_Point_Calculator/Class_Point_Calculator.py
@@ -1,8 +1,8 @@
-team1 = 120
-team2 = 95
-team3 = 140
-team4 = 110
-team5 = 85
+team1 = int(input("Enter Team1 points: "))
+team2 = int(input("Enter Team2 points: "))
+team3 = int(input("Enter Team3 points: "))
+team4 = int(input("Enter Team4 points: "))
+team5 = int(input("Enter Team5 points: "))
  
 total = team1 + team2 + team3 + team4 + team5
 average = total / 5
