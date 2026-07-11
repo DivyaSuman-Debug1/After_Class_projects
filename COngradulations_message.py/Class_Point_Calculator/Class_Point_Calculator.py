0231ec6e59c6c10528efebb8a65cc6881dd83e14
Divya Suman <divya2suman@gmail.com>
2026-07-11 11:41:02 +0400

    Done11/7/2026

---
+++ COngradulations_message.py/Class_Point_Calculator/Class_Point_Calculator.py
@@ -0,0 +1,11 @@
+team1 = 120
+team2 = 95
+team3 = 140
+team4 = 110
+team5 = 85
+ 
+total = team1 + team2 + team3 + team4 + team5
+average = total / 5
+ 
+print("Total points :", total)
+print("Average per team :", average)
