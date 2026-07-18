2376c25faa725c2849b63a69499f9f29a354f2df
Divya Suman <divya2suman@gmail.com>
2026-07-18 12:22:09 +0400

    DOne 18/7/2026

---
+++ My_Daily_Routine/My_Daily_Routine.py
@@ -0,0 +1,6 @@
+str(input("Print daily routine ? (yes/no): "))
+if "yes":
+    print("I wake up at 6 AM.")
+    print("I go to school at 7:20 AM.")
+    print("I play with my friends in the evening.")
+    print("I study for 2 hours at night.")
