bfd9c7a7a2d16956e576b22a4161644934f55262
Divya Suman <divya2suman@gmail.com>
2026-07-18 13:05:37 +0400

    DOne 18/7/2026

---
+++ Number_Swap/Number_Swap.py
@@ -0,0 +1,15 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+print("Numbers Before swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
+
+a, b, c = c, a, b
+
+print("Numbers After swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
